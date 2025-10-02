68b21a89f7b340ee53f8fea7549cf8c6d57d8b7e
AlcivanPereira <alcivanpereira12@gmail.com>
2025-10-01 20:53:48 -0400

    Variaveis e I/O

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,8 +1,37 @@
-﻿
-Console.WriteLine("Hello, World!");
-#region meu primeiro programa
+﻿#region meu primeiro programa
+//Console.WriteLine("Hello, World!");
+#endregion
 
-Console.WriteLine("Hello, World!");
+#region Tipos de variaveis em C#
+// Tipos de variaveis em C#
+
+/* using System.Linq.Expressions;
+
+int idade = 30;   // posso começar declarando a variavel sem atribuir na mesma linha
+
+
+double altura = 1,75
+double peso = 
+ */
+
+/* string nome = "Alcivan";
+
+Console.WriteLine($"Seu nome é{nome}"); */
+
+
+
+/* bool estudantes //Exemplo "true (Verdadeiro) False (Falso)
+bool estudantes = true 
+bool estudantes = false
+boot valor = 2>3; */
+
 
 #endregion
 
+#region 
+Console.WriteLine("Digite o seu nome");
+string nomeImput = Console.ReadLine()!;
+Console.WriteLine($"Seu nome é{nomeImput}");
+
+
+#endregion
